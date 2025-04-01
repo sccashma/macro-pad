dbed2b30d3eefab841cae06f2aba59b0bcc8bbaf
sccashma <scottc2789@gmail.com>
2025-04-01 17:05:13 +0800

    Added debounce to the touchscreen

--- tft_touch.h
+++ tft_touch.h
@@ -6,26 +6,45 @@
 #include <TouchScreen.h>
 #include "touchscreen_calibration.h"
 
-// Touch screen calibration values
-int constexpr MINPRESSURE = 200;
-int constexpr MAXPRESSURE = 1000;
-
 // Pin interface definitions
 #define XP 8
 #define XM A2
 #define YP A3
 #define YM 9
 
-// Global instance of the touch screen
-// For more precise calibration (should not be necessary), follow the instructions provided in the
-//   Adafruit Touchscreen library
-//   Note: The touchscreen calibration process is quite involved and requires a multimeter.
+/// @brief Touch screen calibration values
+int constexpr MINPRESSURE = 200;
+int constexpr MAXPRESSURE = 1000;
+
+/// @brief Debounce parameters
+unsigned long last_pressed_ms = millis();
+unsigned long constexpr DEBOUNCE_THRESHOLD_MS = 300;
+
+/// @brief Global instance of the touch screen
+/// @note For more precise calibration (should not be necessary), follow the instructions provided in the Adafruit Touchscreen library
+/// @note The touchscreen calibration process is quite involved and requires a multimeter.
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+/// @brief Debounce the button press
+/// @return bool: True if the button was pressed, false otherwise
+bool debounce()
+{
+    bool pressed = false;
+    // software debounce
+    unsigned long pressed_ms = millis();
+    
+    if ((pressed_ms - last_pressed_ms) > DEBOUNCE_THRESHOLD_MS)
+    {      
+            last_pressed_ms = pressed_ms;
+            pressed = true;
+    }
+    
+    return pressed;
+}
 
 /// @brief Checks if the screen was touched and updates the TouchPoint
 /// @param tp If the screen was pressed, the x and y coordinates are updated to closely match the pixels
 /// @return True if the screen was pressed, false otherwise
-/// TODO: Add debounce to the touch screen
 bool touched(TSPoint& tp)
 {
     tp = ts.getPoint();  //tp.x, tp.y are raw ADC values
@@ -33,7 +52,7 @@ bool touched(TSPoint& tp)
     pinMode(XM, OUTPUT);
     pinMode(YP, OUTPUT);
 
-    if (tp.z > MINPRESSURE && tp.z < MAXPRESSURE)
+    if (tp.z > MINPRESSURE && tp.z < MAXPRESSURE && debounce())
     {
 #ifdef TOUCH_CALIBRATION_PROCESS
         calibrateTouchscreen(tp);
