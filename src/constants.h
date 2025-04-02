b92ba85b8c00172f5561ceb91dd0e66e1ba459c8
sccashma <scottc2789@gmail.com>
2025-04-02 20:49:59 +0800

    add code parsing to macro

--- src/constants.h
+++ src/constants.h
@@ -1,15 +1,31 @@
+/*
+ * macro.h
+ *
+ * Created: 2/4/2025
+ * Author: Scott Cashman
+*/
+
 #ifndef __DEFAULTS_H__
 #define __DEFAULTS_H__
 
+
+/// @brief The maximum number of key codes in a macro
+/// @note This is arbitrary, but should be large enough to hold the largest macro. 32x8bit = 256 bits = 32 bytes = 32 characters.
+uint8_t constexpr KEY_CODES_MAX = 32;
+
+/// @brief Delay time between key presses.
+/// @note Some applications are sensitive to the time between key presses. You may need to adjust this value to suit your application.
+int constexpr KEYBOARD_ENTRY_DELAY_MS = 50;
+
 ////////////////////////////////////////////////////
 // colour pallette
-static int constexpr RICH_BLACK         = 0x0042;
-static int constexpr INDIGO_DYE         = 0x21ea;
-static int constexpr UCLA_BLUE          = 0x4393;
-static int constexpr ARYLIDE_YELLOW     = 0xde6a;
-static int constexpr BATTLESHIP_GREY    = 0x9cd3;
-static int constexpr SILVER             = 0xc638;
-static int constexpr ANTI_FLASH_WHITE   = 0xef7d;
+int constexpr RICH_BLACK         = 0x0042;
+int constexpr INDIGO_DYE         = 0x21ea;
+int constexpr UCLA_BLUE          = 0x4393;
+int constexpr ARYLIDE_YELLOW     = 0xde6a;
+int constexpr BATTLESHIP_GREY    = 0x9cd3;
+int constexpr SILVER             = 0xc638;
+int constexpr ANTI_FLASH_WHITE   = 0xef7d;
 
 ////////////////////////////////////////////////////
 // Menu states and state transitions
@@ -25,39 +41,34 @@ enum class menu_state_t
 
 ////////////////////////////////////////////////////
 // Default values for buttons
-static int constexpr DEFAULT_BUTTON_CORNER_RADIUS = 12; // Corner radius of the buttons
-
-static int constexpr HOME_SCREEN_BUTTONS = 8; // Number of macros on the home screen (SETTINGS BUTTON IS LAST BUTTON)
-static int constexpr MAIN_MENU_BUTTONS = 2; // Number of buttons in the main menu
-static int constexpr MACRO_SELECT_BUTTONS = 9; // Number of buttons in the macro select menu
-static int constexpr MACRO_SELECT_OPTIONS = 6; // Number of options in the macro select menu
-static int constexpr MACRO_PLACE_OPTIONS = 7; // Number of placement options in the macro place menu
-static int constexpr MACRO_PLACE_BUTTONS = HOME_SCREEN_BUTTONS; // Number of buttons in the macro place menu (last button is save/return)
-
-static int constexpr DEFAULT_MACRO_BUTTON_WIDTH = 80; // Width of the macro buttons on the home screen
-static int constexpr DEFAULT_MACRO_BUTTON_HEIGHT = 80; // Height of the macro buttons on the home screen
-static int constexpr DEFAULT_MENU_BUTTON_WIDTH = 106; // Width of the buttons in the main menu
-static int constexpr DEFAULT_MENU_BUTTON_HEIGHT = 60; // Height of the buttons in the main menu
-
-static int constexpr HOME_SCREEN_ROW_1_Y = 0; // Y coordinate of the first row of buttons on the home screen
-static int constexpr HOME_SCREEN_ROW_2_Y = DEFAULT_MACRO_BUTTON_HEIGHT * 2; // Y coordinate of the second row of buttons on the home screen
-static int constexpr HOME_SCREEN_COL_1_X = 0; // X coordinate of the first column of buttons on the home screen
-static int constexpr HOME_SCREEN_COL_2_X = DEFAULT_MACRO_BUTTON_WIDTH * 2; // X coordinate of the second column of buttons on the home screen
-static int constexpr HOME_SCREEN_COL_3_X = DEFAULT_MACRO_BUTTON_WIDTH * 3; // X coordinate of the third column of buttons on the home screen
-static int constexpr HOME_SCREEN_COL_4_X = DEFAULT_MACRO_BUTTON_WIDTH * 4; // X coordinate of the fourth column of buttons on the home screen
-
-static int constexpr MACRO_SELECT_OPTION_WIDTH = 320; // Width of the macro select options
-static int constexpr MACRO_SELECT_OPTION_HEIGHT = 30; // Height of the macro select options
-static int constexpr MACRO_SELECT_OPTION_1_Y = 0; // Y coordinate of the first macro select option
-static int constexpr MACRO_SELECT_OPTION_2_Y = MACRO_SELECT_OPTION_HEIGHT; // Y coordinate of the second macro select option
-static int constexpr MACRO_SELECT_OPTION_3_Y = MACRO_SELECT_OPTION_HEIGHT * 2; // Y coordinate of the third macro select option
-static int constexpr MACRO_SELECT_OPTION_4_Y = MACRO_SELECT_OPTION_HEIGHT * 3; // Y coordinate of the fourth macro select option
-static int constexpr MACRO_SELECT_OPTION_5_Y = MACRO_SELECT_OPTION_HEIGHT * 4; // Y coordinate of the fifth macro select option
-static int constexpr MACRO_SELECT_OPTION_6_Y = MACRO_SELECT_OPTION_HEIGHT * 5; // Y coordinate of the sixth macro select option
-
-static int constexpr MACRO_SELECT_SCROLL_LEFT_X = 1; // X coordinate of the scroll left button in the macro select menu
-static int constexpr MACRO_SELECT_SCROLL_RIGHT_X = MACRO_SELECT_OPTION_WIDTH + 1; // X coordinate of the scroll right button in the macro select menu
-static int constexpr MACRO_SELECT_SCROLL_Y = MACRO_SELECT_OPTIONS * MACRO_SELECT_OPTION_HEIGHT; // Y coordinate of the scroll buttons in the macro select menu
+int constexpr DEFAULT_BUTTON_CORNER_RADIUS = 12; // Corner radius of the buttons
+int constexpr HOME_SCREEN_BUTTONS = 8; // Number of macros on the home screen (SETTINGS BUTTON IS LAST BUTTON)
+int constexpr MAIN_MENU_BUTTONS = 2; // Number of buttons in the main menu
+int constexpr MACRO_SELECT_BUTTONS = 9; // Number of buttons in the macro select menu
+int constexpr MACRO_SELECT_OPTIONS = 6; // Number of options in the macro select menu
+int constexpr MACRO_PLACE_OPTIONS = 7; // Number of placement options in the macro place menu
+int constexpr MACRO_PLACE_BUTTONS = HOME_SCREEN_BUTTONS; // Number of buttons in the macro place menu (last button is save/return)
+int constexpr DEFAULT_MACRO_BUTTON_WIDTH = 80; // Width of the macro buttons on the home screen
+int constexpr DEFAULT_MACRO_BUTTON_HEIGHT = 80; // Height of the macro buttons on the home screen
+int constexpr DEFAULT_MENU_BUTTON_WIDTH = 106; // Width of the buttons in the main menu
+int constexpr DEFAULT_MENU_BUTTON_HEIGHT = 60; // Height of the buttons in the main menu
+int constexpr HOME_SCREEN_ROW_1_Y = 0; // Y coordinate of the first row of buttons on the home screen
+int constexpr HOME_SCREEN_ROW_2_Y = DEFAULT_MACRO_BUTTON_HEIGHT * 2; // Y coordinate of the second row of buttons on the home screen
+int constexpr HOME_SCREEN_COL_1_X = 0; // X coordinate of the first column of buttons on the home screen
+int constexpr HOME_SCREEN_COL_2_X = DEFAULT_MACRO_BUTTON_WIDTH * 2; // X coordinate of the second column of buttons on the home screen
+int constexpr HOME_SCREEN_COL_3_X = DEFAULT_MACRO_BUTTON_WIDTH * 3; // X coordinate of the third column of buttons on the home screen
+int constexpr HOME_SCREEN_COL_4_X = DEFAULT_MACRO_BUTTON_WIDTH * 4; // X coordinate of the fourth column of buttons on the home screen
+int constexpr MACRO_SELECT_OPTION_WIDTH = 320; // Width of the macro select options
+int constexpr MACRO_SELECT_OPTION_HEIGHT = 30; // Height of the macro select options
+int constexpr MACRO_SELECT_OPTION_1_Y = 0; // Y coordinate of the first macro select option
+int constexpr MACRO_SELECT_OPTION_2_Y = MACRO_SELECT_OPTION_HEIGHT; // Y coordinate of the second macro select option
+int constexpr MACRO_SELECT_OPTION_3_Y = MACRO_SELECT_OPTION_HEIGHT * 2; // Y coordinate of the third macro select option
+int constexpr MACRO_SELECT_OPTION_4_Y = MACRO_SELECT_OPTION_HEIGHT * 3; // Y coordinate of the fourth macro select option
+int constexpr MACRO_SELECT_OPTION_5_Y = MACRO_SELECT_OPTION_HEIGHT * 4; // Y coordinate of the fifth macro select option
+int constexpr MACRO_SELECT_OPTION_6_Y = MACRO_SELECT_OPTION_HEIGHT * 5; // Y coordinate of the sixth macro select option
+int constexpr MACRO_SELECT_SCROLL_LEFT_X = 1; // X coordinate of the scroll left button in the macro select menu
+int constexpr MACRO_SELECT_SCROLL_RIGHT_X = MACRO_SELECT_OPTION_WIDTH + 1; // X coordinate of the scroll right button in the macro select menu
+int constexpr MACRO_SELECT_SCROLL_Y = MACRO_SELECT_OPTIONS * MACRO_SELECT_OPTION_HEIGHT; // Y coordinate of the scroll buttons in the macro select menu
 
 
 #endif // __DEFAULTS_H__
--- src/macro.h
+++ src/macro.h
@@ -1,7 +1,7 @@
 /*
  * macro.h
  *
- * Created: 3/10/2020 10:51:52 AM
+ * Created: 1/3/2025
  * Author: Scott Cashman
 */
 
@@ -12,13 +12,12 @@
 #include "key_map.h"
 #include "csv_parser.h"
 #include "sd_utils.h"
+#include "constants.h"
 
 namespace macro
 {
 
-/// @brief The maximum number of key codes in a macro
-/// @note This is arbitrary, but should be large enough to hold the largest macro. 32x8bit = 256 bits = 32 bytes = 32 characters.
-uint8_t constexpr KEY_CODES_MAX = 32;
+size_t parseKeyCodes(String const &, uint8_t *, size_t const );
 
 /// @brief Data structure defining a macro
 /// @note The macro is defined as a sequence of keys. The key codes are defined in the key_map.h file.
@@ -28,6 +27,17 @@ typedef struct macro_t
     String name;                                ///< The name of the macro
     size_t const codes_size = KEY_CODES_MAX;    ///< The size of the key codes array
     uint8_t codes[KEY_CODES_MAX];               ///< The key codes for the macro
+
+    /// @brief constructor for the macro_t type
+    macro_t()
+    {
+        this->id = NULL;
+        this->name = "";
+        for (size_t i = 0; i < this->codes_size; i++)
+        {
+            this->codes[i] = 0;
+        }
+    }
 
     /// @brief operator overload for the assignment operator
     /// @param other The macro to assign to this macro
@@ -41,6 +51,29 @@ typedef struct macro_t
             this->codes[i] = other.codes[i];
         }
         return *this;
+    }
+
+    /// @brief setup the macro's code
+    /// @param keys The key sequence to generate a macro from
+    /// @return size_t: The number of key codes generated
+    size_t initialiseCodes(String const &keys)
+    {
+        size_t idx = parseKeyCodes(keys, this->codes, this->codes_size);
+        this->codes[idx] = 0; // Null terminate the array
+        return idx;
+    }
+
+    /// @brief play the macro
+    /// @note This function will send the key codes to the keyboard in the order they are defined in the macro
+    /// @todo Add functionality to send multiple keys at once (e.g. CTRL + C)
+    void play() const
+    {
+        for (size_t i = 0; i < this->codes_size; i++)
+        {
+            if (this->codes[i] == 0) break; // Stop if we reach the end of the macro
+            Keyboard.write(this->codes[i]);
+            delay(KEYBOARD_ENTRY_DELAY_MS); // Delay between key presses
+        }
     }
 
 } macro_t;
