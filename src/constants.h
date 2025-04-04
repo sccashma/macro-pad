47862ce0bb2317e5ed5274ade3b81a20c3b0ac1d
sccashma <scottc2789@gmail.com>
2025-04-04 22:08:50 +0800

    updated wireframe

--- src/constants.h
+++ src/constants.h
@@ -42,38 +42,46 @@ enum class menu_state_t
 
 ////////////////////////////////////////////////////
 // Numbers of buttons
-int constexpr HOME_SCREEN_BUTTONS = 8; // Number of macros on the home screen (SETTINGS BUTTON IS LAST BUTTON)
-int constexpr MAIN_MENU_BUTTONS = 2; // Number of buttons in the main menu
-int constexpr MACRO_SELECT_BUTTONS = 9; // Number of buttons in the macro select menu
-int constexpr MACRO_SELECT_OPTIONS = 6; // Number of options in the macro select menu
-int constexpr MACRO_PLACE_OPTIONS = 7; // Number of placement options in the macro place menu
-int constexpr MACRO_PLACE_BUTTONS = HOME_SCREEN_BUTTONS; // Number of buttons in the macro place menu (last button is save/return)
+uint16_t constexpr HOME_SCREEN_BUTTONS = 8; // Number of macros on the home screen (SETTINGS BUTTON IS LAST BUTTON)
+uint16_t constexpr MAIN_MENU_BUTTONS = 2; // Number of buttons in the main menu
+uint16_t constexpr MACRO_SELECT_BUTTONS = 9; // Number of buttons in the macro select menu
+uint16_t constexpr MACRO_SELECT_OPTIONS = 6; // Number of options in the macro select menu
+uint16_t constexpr MACRO_PLACE_OPTIONS = 7; // Number of placement options in the macro place menu
+uint16_t constexpr MACRO_PLACE_BUTTONS = HOME_SCREEN_BUTTONS; // Number of buttons in the macro place menu (last button is save/return)
 
 // Button sizes/drawing parameters
-int constexpr DEFAULT_BUTTON_CORNER_RADIUS = 12; // Corner radius of the buttons
-int constexpr DEFAULT_MACRO_BUTTON_WIDTH = 80; // Width of the macro buttons on the home screen
-int constexpr DEFAULT_MACRO_BUTTON_HEIGHT = 80; // Height of the macro buttons on the home screen
-int constexpr DEFAULT_MENU_BUTTON_WIDTH = 106; // Width of the buttons in the main menu
-int constexpr DEFAULT_MENU_BUTTON_HEIGHT = 60; // Height of the buttons in the main menu
-int constexpr MACRO_SELECT_OPTION_WIDTH = 320; // Width of the macro select options
-int constexpr MACRO_SELECT_OPTION_HEIGHT = 30; // Height of the macro select options
+uint16_t constexpr DEFAULT_BUTTON_CORNER_RADIUS = 12; // Corner radius of the buttons
+uint16_t constexpr DEFAULT_MACRO_BUTTON_WIDTH = 80; // Width of the macro buttons on the home screen
+uint16_t constexpr DEFAULT_MACRO_BUTTON_HEIGHT = 80; // Height of the macro buttons on the home screen
+uint16_t constexpr DEFAULT_MENU_BUTTON_WIDTH = 106; // Width of the buttons in the main menu
+uint16_t constexpr DEFAULT_MENU_BUTTON_HEIGHT = 60; // Height of the buttons in the main menu
+uint16_t constexpr MACRO_SELECT_OPTION_WIDTH = 320; // Width of the macro select options
+uint16_t constexpr MACRO_SELECT_OPTION_HEIGHT = 30; // Height of the macro select options
 
-// Button locations
-int constexpr HOME_SCREEN_ROW_1_Y = 0; // Y coordinate of the first row of buttons on the home screen
-int constexpr HOME_SCREEN_ROW_2_Y = DEFAULT_MACRO_BUTTON_HEIGHT * 2; // Y coordinate of the second row of buttons on the home screen
-int constexpr HOME_SCREEN_COL_1_X = 0; // X coordinate of the first column of buttons on the home screen
-int constexpr HOME_SCREEN_COL_2_X = DEFAULT_MACRO_BUTTON_WIDTH * 1; // X coordinate of the second column of buttons on the home screen
-int constexpr HOME_SCREEN_COL_3_X = DEFAULT_MACRO_BUTTON_WIDTH * 2; // X coordinate of the third column of buttons on the home screen
-int constexpr HOME_SCREEN_COL_4_X = DEFAULT_MACRO_BUTTON_WIDTH * 3; // X coordinate of the fourth column of buttons on the home screen
-int constexpr MACRO_SELECT_OPTION_1_Y = 0; // Y coordinate of the first macro select option
-int constexpr MACRO_SELECT_OPTION_2_Y = MACRO_SELECT_OPTION_HEIGHT; // Y coordinate of the second macro select option
-int constexpr MACRO_SELECT_OPTION_3_Y = MACRO_SELECT_OPTION_HEIGHT * 2; // Y coordinate of the third macro select option
-int constexpr MACRO_SELECT_OPTION_4_Y = MACRO_SELECT_OPTION_HEIGHT * 3; // Y coordinate of the fourth macro select option
-int constexpr MACRO_SELECT_OPTION_5_Y = MACRO_SELECT_OPTION_HEIGHT * 4; // Y coordinate of the fifth macro select option
-int constexpr MACRO_SELECT_OPTION_6_Y = MACRO_SELECT_OPTION_HEIGHT * 5; // Y coordinate of the sixth macro select option
-int constexpr MACRO_SELECT_SCROLL_LEFT_X = 1; // X coordinate of the scroll left button in the macro select menu
-int constexpr MACRO_SELECT_SCROLL_RIGHT_X = MACRO_SELECT_OPTION_WIDTH + 1; // X coordinate of the scroll right button in the macro select menu
-int constexpr MACRO_SELECT_SCROLL_Y = MACRO_SELECT_OPTIONS * MACRO_SELECT_OPTION_HEIGHT; // Y coordinate of the scroll buttons in the macro select menu
+// Home screen Button locations
+uint16_t constexpr HOME_SCREEN_ROW_1_Y = 0; // Y coordinate of the first row of buttons on the home screen
+uint16_t constexpr HOME_SCREEN_ROW_2_Y = DEFAULT_MACRO_BUTTON_HEIGHT * 2; // Y coordinate of the second row of buttons on the home screen
+uint16_t constexpr HOME_SCREEN_COL_1_X = 0; // X coordinate of the first column of buttons on the home screen
+uint16_t constexpr HOME_SCREEN_COL_2_X = DEFAULT_MACRO_BUTTON_WIDTH * 1; // X coordinate of the second column of buttons on the home screen
+uint16_t constexpr HOME_SCREEN_COL_3_X = DEFAULT_MACRO_BUTTON_WIDTH * 2; // X coordinate of the third column of buttons on the home screen
+uint16_t constexpr HOME_SCREEN_COL_4_X = DEFAULT_MACRO_BUTTON_WIDTH * 3; // X coordinate of the fourth column of buttons on the home screen
 
+// Main menu button locations
+uint16_t constexpr MAIN_MENU_ROW_Y = 90; // Y coordinate of the first row of buttons in the main menu
+uint16_t constexpr MAIN_MENU_COL_1_X = 36; // X coordinate of the first column of buttons in the main menu
+uint16_t constexpr MAIN_MENU_COL_2_X = DEFAULT_MENU_BUTTON_WIDTH + (2*36); // X coordinate of the second column of buttons in the main menu
+
+// Macro selection menu button locations
+uint16_t constexpr MACRO_SELECT_OPTION_X = 0; // X coordinate of the options
+uint16_t constexpr MACRO_SELECT_OPTION_1_Y = 0; // Y coordinate of the first macro select option
+uint16_t constexpr MACRO_SELECT_OPTION_2_Y = MACRO_SELECT_OPTION_HEIGHT; // Y coordinate of the second macro select option
+uint16_t constexpr MACRO_SELECT_OPTION_3_Y = MACRO_SELECT_OPTION_HEIGHT * 2; // Y coordinate of the third macro select option
+uint16_t constexpr MACRO_SELECT_OPTION_4_Y = MACRO_SELECT_OPTION_HEIGHT * 3; // Y coordinate of the fourth macro select option
+uint16_t constexpr MACRO_SELECT_OPTION_5_Y = MACRO_SELECT_OPTION_HEIGHT * 4; // Y coordinate of the fifth macro select option
+uint16_t constexpr MACRO_SELECT_OPTION_6_Y = MACRO_SELECT_OPTION_HEIGHT * 5; // Y coordinate of the sixth macro select option
+uint16_t constexpr MACRO_SELECT_SCROLL_LEFT_X = 0; // X coordinate of the scroll left button in the macro select menu
+uint16_t constexpr MACRO_SELECT_SCROLL_RIGHT_X = DEFAULT_MENU_BUTTON_WIDTH + 1; // X coordinate of the scroll right button in the macro select menu
+uint16_t constexpr MACRO_SELECT_BACK_X = MACRO_SELECT_SCROLL_RIGHT_X + DEFAULT_MENU_BUTTON_WIDTH + 1; // X coordinate of the back button in the macro select menu
+uint16_t constexpr MACRO_SELECT_SCROLL_Y = MACRO_SELECT_OPTIONS * MACRO_SELECT_OPTION_HEIGHT; // Y coordinate of the scroll buttons in the macro select menu
 
 #endif // __DEFAULTS_H__
